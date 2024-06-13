f1a6fad6b7af7caec8976f54ac4114133fd9689f
Ishusandhu <133061953+Ishusandhu@users.noreply.github.com>
2024-06-13 17:20:35 -0400

    Made a method to get Content from servies

--- IshuSandhuMyFirstAngularApplication/src/app/service/fetch-cars.service.ts
+++ IshuSandhuMyFirstAngularApplication/src/app/service/fetch-cars.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { IContent } from '../interface';
+import { CONTENT } from '../data/mock-content';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchCarsService {
-
+  private contet: IContent[] = CONTENT;
   constructor() { }
+  getContent():Observable<IContent[]>{
+    return of(this.contet);
+  }
 }
