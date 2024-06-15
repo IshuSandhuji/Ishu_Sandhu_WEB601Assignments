0909d81058dd65987eb9a5cab2468f8976291947
Ishusandhu <133061953+Ishusandhu@users.noreply.github.com>
2024-06-14 21:12:12 -0400

    method added

--- IshuSandhuMyFirstAngularApplication/src/app/service/fetch-cars.service.ts
+++ IshuSandhuMyFirstAngularApplication/src/app/service/fetch-cars.service.ts
@@ -8,8 +8,28 @@ import { Observable, of } from 'rxjs';
 })
 export class FetchCarsService {
   private contet: IContent[] = CONTENT;
+  private oneContet: IContent | undefined;
   constructor() { }
-  getContent():Observable<IContent[]>{
+  getContent(): Observable<IContent[]> {
     return of(this.contet);
   }
+  getIdData(id: number): Observable<IContent | undefined> {
+    this.oneContet = this.contet.find((c, i) => c.id == id);
+    return of(this.oneContet)
+  }
+  addContent(item: IContent): Observable<IContent[] | undefined> {
+    this.contet.push(item);
+    return of(this.contet);
+  }
+  updateContent(item: IContent): Observable<IContent | undefined> {
+    this.oneContet = item;
+    this.contet.push(this.oneContet);
+    return of(this.oneContet);
+  }
+
+  removeContent(id: number): Observable<IContent[]|undefined> {
+  this.contet = this.contet.filter((c, i) => c.id !== id);
+  return of(this.contet);
+  }
+
 }
