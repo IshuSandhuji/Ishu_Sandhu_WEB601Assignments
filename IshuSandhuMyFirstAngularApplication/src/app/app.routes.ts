92f2c5da46f5710f0b4fc203f8a526e7c71d4c59
Ishusandhu <133061953+Ishusandhu@users.noreply.github.com>
2024-07-04 20:28:06 -0400

    routing done

--- IshuSandhuMyFirstAngularApplication/src/app/app.routes.ts
+++ IshuSandhuMyFirstAngularApplication/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:"content",
+        pathMatch: 'prefix'
+    },
+    {
+        path:"content",
+        component:ContentListComponent,
+
+    }
+];
